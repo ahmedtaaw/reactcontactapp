b4ad585a696956f273ac17253c3300d3f47a1a0d
tawfik <ahmedtaaw@gmail.com>
2019-06-16 00:01:26 +0400

    display data contact props

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 
 class ListContacts extends Component{
     render(){
+        console.log(this.props);
         return(
             <ol className="contact-list">
-
+                {
+                    this.props.contacts.map((contact)=>(
+                        <li key={contact.name}>
+                            {contact.name}
+                        </li>
+                    ))
+                }
             </ol>
         )
     }
