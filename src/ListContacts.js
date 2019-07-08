6f62d7a3fbf58cd3e9eeab96676885693e391e88
tawfik <ahmedtaaw@gmail.com>
2019-07-08 07:06:29 +0400

    state

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -7,57 +7,60 @@ class ListContacts extends Component {
         onDeleteContact: propTypes.func.isRequired,
     }
 
-    state={
-        query:''
+    state = {
+        query: ''
     }
 
     //updateQuery method which will take query
-    updateQuery=(query)=>{
-        this.setState(()=>({
+    updateQuery = (query) => {
+        this.setState(() => ({
             query: query.trim()
         }))
     }
 
     render() {
+        const {query} = this.state
+        const {contacts, onDeleteContact} = this.props
+
         return (
             <div className="list-contacts">
                 {
                     //in order to see state while typing like console.log
-                    JSON.stringify(this.state)}
-                <div className='list-contacts-top'>
-                    <input
-                        className='search-contacts'
-                        type='text'
-                        placeholder='Search Contacts'
-                        value={this.state.query}
-                        onChange={(event)=>this.updateQuery(event.target.value)}
-                    />
-                </div>
-                <ol className="contact-list">
-                    {
-                        this.props.contacts.map((contact) => (
-                            <li key={contact.name} className='contact-list-item'>
-                                <div
-                                    className='contact-avatar'
-                                    style={{
-                                        backgroundImage: `url(${contact.avatarURL})`
-                                    }}
-                                ></div>
-                                <div className="contact-details">
-                                    <p>{contact.name}</p>
-                                    <p>@{contact.handle}</p>
-                                </div>
-                                <button
-                                    onClick={() => this.props.onDeleteContact(contact)}
-                                    className='contact-remove'>
-                                    Remove
-                            </button>
-                            </li>
-                        ))
-                    }
-                </ol>
+                    JSON.stringify(query)}
+                    <div className='list-contacts-top'>
+                        <input
+                            className='search-contacts'
+                            type='text'
+                            placeholder='Search Contacts'
+                            value={query}
+                            onChange={(event) => this.updateQuery(event.target.value)}
+                        />
+                    </div>
+                    <ol className="contact-list">
+                        {
+                            contacts.map((contact) => (
+                                <li key={contact.name} className='contact-list-item'>
+                                    <div
+                                        className='contact-avatar'
+                                        style={{
+                                            backgroundImage: `url(${contact.avatarURL})`
+                                        }}
+                                    ></div>
+                                    <div className="contact-details">
+                                        <p>{contact.name}</p>
+                                        <p>@{contact.handle}</p>
+                                    </div>
+                                    <button
+                                        onClick={() => onDeleteContact(contact)}
+                                        className='contact-remove'>
+                                        Remove
+                                </button>
+                                </li>
+                            ))
+                        }
+                    </ol>
 
-            </div>
+                </div>
         )
     }
 }
